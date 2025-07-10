077503dd5e1148da919a3901276adf2c253778f8
rktlskan021 <rktlskan021@gmail.com>
2025-07-10 14:23:35 +0900

    Refactor: header 배경색 변경

--- datacenter/src/components/Header.js
+++ datacenter/src/components/Header.js
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <header className="flex justify-center items-center h-24 w-full shadow-sm border-b border-gray-200">
+        <header className="flex justify-center items-center h-24 w-full shadow-sm border-b border-gray-200 bg-white">
             <div className="flex max-w-[90%] w-full justify-between">
                 <h1 className="text-4xl font-sans font-bold text-gray-900">Data Center</h1>
                 <button className="font-bold bg-blue-600 hover:bg-blue-700 rounded-md text-white text-xm px-5 py-3">
