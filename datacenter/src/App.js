4e810eedd9c4962cff0dd028c871e87866fcaedb
rktlskan021 <rktlskan021@gmail.com>
2025-07-10 11:16:42 +0900

    Feat: 라우팅 세팅

--- datacenter/src/App.js
+++ datacenter/src/App.js
@@ -1,7 +1,16 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Header from './components/Header';
 
-function App() {
-    return <div className="App"></div>;
-}
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
 
 export default App;
