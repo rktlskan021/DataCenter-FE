9d04022e6851d7b521c8850a16c9caff98b01ba4
rktlskan021 <rktlskan021@gmail.com>
2025-07-14 17:37:32 +0900

    Fix: 유저 로그인 후 새로고침 시 Header admin으로 적용되는 버그 수정

--- datacenter/src/stores/useAuthStore.js
+++ datacenter/src/stores/useAuthStore.js
@@ -6,19 +6,20 @@ const useAuthStore = create((set) => ({
     name: Cookies.get('name') || null,
     token: Cookies.get('token') || null, // 쿠키에서 초기값 읽음
     isLoggedIn: !!Cookies.get('token'),
-    isAdmin: !!Cookies.get('isAdmin'),
+    isAdmin: Cookies.get('isAdmin') === 'true',
 
     login: (userInfo, token) => {
         Cookies.set('token', token, { expires: 7 }); // 쿠키 저장 (7일 유효)
         Cookies.set('name', userInfo.name, { expires: 7 }); // 쿠키 저장 (7일 유효)
-        Cookies.set('isAdmin', userInfo.isAdmin, { expires: 7 }); // 쿠키 저장 (7일 유효)
-        set({ name: userInfo.name, token, isLoggedIn: true, admin: userInfo.isAdmin });
+        Cookies.set('isAdmin', userInfo.isAdmin ? 'true' : 'false', { expires: 7 }); // 쿠키 저장 (7일 유효)
+        set({ name: userInfo.name, token, isLoggedIn: true, isAdmin: userInfo.isAdmin });
     },
 
     logout: () => {
         Cookies.remove('token'); // 쿠키 삭제
         Cookies.remove('name'); // 쿠키 삭제
-        set({ user: null, token: null, isLoggedIn: false });
+        Cookies.remove('isAdmin'); // 쿠키 삭제
+        set({ user: null, token: null, isLoggedIn: false, isAdmin: null });
     },
 }));
 
