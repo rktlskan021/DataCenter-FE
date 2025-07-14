0719e6d93af69d27591f75a7c7d91a1babd2c808
rktlskan021 <rktlskan021@gmail.com>
2025-07-14 17:37:07 +0900

    design: button 태그 속 Link 꺼냄

--- datacenter/src/components/Header.js
+++ datacenter/src/components/Header.js
@@ -1,10 +1,8 @@
 import { Link } from 'react-router-dom';
 import useAuthStore from '../stores/useAuthStore';
-import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
     const { name, isLoggedIn, logout, isAdmin } = useAuthStore();
-    const navigator = useNavigate();
 
     return (
         <header className="font-sans flex justify-center items-center h-24 w-full shadow-sm border-b border-gray-200 bg-white">
@@ -16,13 +14,19 @@ export default function Header() {
                     <div className="flex gap-3 items-center">
                         <span>안녕하세요, {name}님</span>
                         {isAdmin ? (
-                            <button className="font-bold bg-white hover:bg-gray-300 border border-gray-200 rounded-md text-black text-xm px-5 py-3 transition duration-200 ease-in-out">
-                                <Link to="/admin">Admin Page</Link>
-                            </button>
+                            <Link
+                                to="/admin"
+                                className="font-bold bg-white hover:bg-gray-300 border border-gray-200 rounded-md text-black text-xm px-5 py-3 transition duration-200 ease-in-out"
+                            >
+                                Admin Page
+                            </Link>
                         ) : (
-                            <button className="font-bold bg-white hover:bg-gray-300 border border-gray-200 rounded-md text-black text-xm px-5 py-3 transition duration-200 ease-in-out">
-                                <Link to="/mypage">Mypage</Link>
-                            </button>
+                            <Link
+                                to="/mypage"
+                                className="font-bold bg-white hover:bg-gray-300 border border-gray-200 rounded-md text-black text-xm px-5 py-3 transition duration-200 ease-in-out"
+                            >
+                                Mypage
+                            </Link>
                         )}
                         <button
                             className="font-bold bg-white hover:bg-gray-300 border border-gray-200 rounded-md text-black text-xm px-5 py-3 transition duration-200 ease-in-out"
@@ -32,12 +36,12 @@ export default function Header() {
                         </button>
                     </div>
                 ) : (
-                    <button
+                    <Link
+                        to="/login"
                         className="font-bold bg-blue-600 hover:bg-blue-700 rounded-md text-white text-xm px-5 py-3 transition duration-200 ease-in-out"
-                        onClick={() => navigator('/login')}
                     >
                         Login
-                    </button>
+                    </Link>
                 )}
             </div>
         </header>
