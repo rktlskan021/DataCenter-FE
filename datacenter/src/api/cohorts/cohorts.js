bc43951c03a50d5379f4d812161b459001a527b3
rktlskan021 <rktlskan021@gmail.com>
2025-07-22 17:03:00 +0900

    Feat: API 수정

--- datacenter/src/api/cohorts/cohorts.js
+++ datacenter/src/api/cohorts/cohorts.js
@@ -1,6 +1,11 @@
 import axiosInstance from '../axios/axiosInstance';
 
 export const fetchCohorts = async () => {
-    const res = await axiosInstance.get('/cohort');
+    const res = await axiosInstance.get('/api/cohort/');
+    return res.data;
+};
+
+export const fetchCohortDetail = async (cohort_id) => {
+    const res = await axiosInstance.get(`/api/cohort/id/${cohort_id}`);
     return res.data;
 };
--- datacenter/src/api/users/users.js
+++ datacenter/src/api/users/users.js
@@ -1,17 +1,5 @@
 import axiosInstance from '../axios/axiosInstance';
 import publicAxios from '../axios/publicAxios';
-import qs from 'qs';
-
-export const fetchUserCohorts = async () => {
-    const res = await axiosInstance.get('/api/user/cohort');
-    return res.data;
-};
-
-export const fetchUserDetailCohort = async (cohort_id) => {
-    console.log(cohort_id);
-    const res = await axiosInstance.get(`/api/user/cohort/id/${cohort_id}`);
-    return res.data;
-};
 
 export const postApplyCohort = async ({ cohort_id }) => {
     const res = await axiosInstance.post(`/api/user/cohort/id/${cohort_id}`);
