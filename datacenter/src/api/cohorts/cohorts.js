8dd166d1b8665217a0fcb94d69186730f817143b
rktlskan021 <rktlskan021@gmail.com>
2025-07-23 11:44:49 +0900

    Feat: 코호트 데이터 접근 권한 신청 API 구현

--- datacenter/src/api/cohorts/cohorts.js
+++ datacenter/src/api/cohorts/cohorts.js
@@ -9,3 +9,27 @@ export const fetchCohortDetail = async (cohort_id) => {
     const res = await axiosInstance.get(`/api/cohort/id/${cohort_id}`);
     return res.data;
 };
+
+export const postApplyCohort = async ({
+    cohort_id,
+    schemaName,
+    schemaDescription,
+    selectedTables,
+    selectedFiles,
+}) => {
+    const formData = new FormData();
+
+    formData.append('name', schemaName);
+    formData.append('description', schemaDescription);
+
+    selectedTables.forEach((table) => {
+        formData.append('tables', table);
+    });
+
+    selectedFiles.forEach((file) => {
+        formData.append('files', file);
+    });
+
+    const res = await axiosInstance.post(`/api/cohort/id/${cohort_id}/apply/`, formData);
+    return res.data;
+};
