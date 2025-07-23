a5f8f758ce70dc00c105f73a0318376ccf28dad0
rktlskan021 <rktlskan021@gmail.com>
2025-07-23 11:45:08 +0900

    Feat: IrbDrb 데이터 Fetch API 구현

--- datacenter/src/api/users/users.js
+++ datacenter/src/api/users/users.js
@@ -1,15 +1,19 @@
 import axiosInstance from '../axios/axiosInstance';
 import publicAxios from '../axios/publicAxios';
 
-export const postApplyCohort = async ({ cohort_id }) => {
-    const res = await axiosInstance.post(`/api/user/cohort/id/${cohort_id}`);
-    return res.data;
-};
-
 export const postLogin = async (id, password) => {
     const res = await publicAxios.post('/login', {
         id: id,
         pw: password,
     });
     return res.data;
+};
+
+export const fetchIrbDrbData = async (path, name) => {
+    const res = await axiosInstance.get(`/documents/${path}`, {
+        responseType: 'blob',
+    });
+
+    const file = new File([res.data], name, { type: res.data.typ });
+    return file;
 };
