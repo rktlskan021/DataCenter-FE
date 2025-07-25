3aaf08cd73348d3616683a5aea82c307fb8fc605
rktlskan021 <rktlskan021@gmail.com>
2025-07-25 17:19:07 +0900

    Feat: 코호트 신청 승인/거부 관련 API, Hook 구현

--- datacenter/src/api/admins/admins.js
+++ datacenter/src/api/admins/admins.js
@@ -4,3 +4,17 @@ export const fetchApplies = async () => {
     const res = await axiosInstance.get('/api/admin/applies');
     return res.data;
 };
+
+export const postApplyApprove = async ({ cohort_id, review }) => {
+    const res = await axiosInstance.post(`/api/admin/applies/id/${cohort_id}/approve`, {
+        review: review,
+    });
+    return res.data;
+};
+
+export const postApplyReject = async ({ cohort_id, review }) => {
+    const res = await axiosInstance.post(`/api/admin/applies/id/${cohort_id}/reject`, {
+        review: review,
+    });
+    return res.data;
+};
--- datacenter/src/hooks/queries/useAdmins.js
+++ datacenter/src/hooks/queries/useAdmins.js
@@ -1,9 +1,21 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import { fetchApplies } from '../../api/admins/admins';
+import { fetchApplies, postApplyApprove, postApplyReject } from '../../api/admins/admins';
 
 export const useApplies = () => {
     return useQuery({
         queryKey: ['applies'],
         queryFn: fetchApplies,
+    });
+};
+
+export const useApplyApproce = () => {
+    return useMutation({
+        mutationFn: postApplyApprove,
+    });
+};
+
+export const useApplyReject = () => {
+    return useMutation({
+        mutationFn: postApplyReject,
     });
 };
