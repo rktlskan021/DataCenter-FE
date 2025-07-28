d4d8a0b2ebf28eba9fd666f2efc7f362f3a7e648
rktlskan021 <rktlskan021@gmail.com>
2025-07-28 11:09:13 +0900

    Feat: 수정 후 재신청 버튼 클릭 시 코호트 디테일 페이지로 이동

--- datacenter/src/components/modals/RejectionModal.js
+++ datacenter/src/components/modals/RejectionModal.js
@@ -1,7 +1,10 @@
 import { useEffect } from 'react';
 import { FaRegTimesCircle } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
 
 export default function RejectionModal({ isModalOpen, setIsModalOpen, app }) {
+    const navigate = useNavigate();
+
     useEffect(() => {
         const handleEsc = (e) => {
             if (e.key === 'Escape') {
@@ -22,18 +25,17 @@ export default function RejectionModal({ isModalOpen, setIsModalOpen, app }) {
                             <FaRegTimesCircle className="h-5 w-5" />
                             <h2 className="text-lg font-semibold">신청 반려 사유</h2>
                         </div>
-                        <p className="text-gray-600 mb-5">
-                            {app.cohortName} 신청이 반려된 사유입니다.
-                        </p>
+                        <p className="text-gray-600 mb-5">{app.name} 신청이 반려된 사유입니다.</p>
                         <h2 className="text-gray-900 font-bold text-lg">반려 사유</h2>
                         <div className="bg-red-50 border border-red-200 rounded-lg p-4">
-                            <p className="text-sm text-red-800 text-justify">
-                                {app.rejectionReason}
-                            </p>
+                            <p className="text-sm text-red-800 text-justify">{app.review}</p>
                         </div>
                         <div className="mt-4 flex justify-end gap-2 font-bold text-xm">
                             <button
-                                onClick={() => setIsModalOpen(false)}
+                                onClick={() => {
+                                    setIsModalOpen(false);
+                                    navigate(`/structured/${app.id}`);
+                                }}
                                 className="px-4 py-2 bg-white border border-gray-200 rounded hover:bg-gray-100"
                             >
                                 수정 후 재신청
