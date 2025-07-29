fb65683d36ccdb562f02fe96ab5f2b64f7d4a017
rktlskan021 <rktlskan021@gmail.com>
2025-07-29 17:38:09 +0900

    Feat: 코호트 신청 성공 처리 및 네비게이션 적용

--- datacenter/src/hooks/queries/useCohorts.js
+++ datacenter/src/hooks/queries/useCohorts.js
@@ -1,5 +1,7 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { fetchCohorts, fetchCohortDetail, postApplyCohort } from '../../api/cohorts/cohorts';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
 export const useCohorts = () => {
     return useQuery({
@@ -17,7 +19,19 @@ export const useCohortDetail = (cohort_id) => {
 };
 
 export const useApplyCohort = () => {
+    const navigate = useNavigate();
     return useMutation({
         mutationFn: postApplyCohort,
+        onSuccess: () => {
+            toast(`성공적으로 신청되었습니다.`, {
+                icon: '📋',
+                className: 'bg-gray-100 text-gray-800 font-medium rounded-md shadow-sm',
+                bodyClassName: 'text-sm whitespace-nowrap max-w-full',
+            });
+            navigate('/home');
+        },
+        onError: (err) => {
+            console.log(err);
+        },
     });
 };
